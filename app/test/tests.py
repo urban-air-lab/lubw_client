8b12f80a3088bb310ac3624407b15f48828c598e
tobias.weiland <tobias.weiland@7p-group.com>
2025-05-19 20:08:25 +0200

    refactore file structure

--- app/test/tests.py
+++ app/test/tests.py
@@ -1,31 +1,30 @@
-
 import requests
 import pandas as pd
 from datetime import datetime
-from app.src import fetch_and_publish_lubw_data
+from app.src.utils import *
 
 
 def test_timestamp_with_offset_types():
-    actual = fetch_and_publish_lubw_data.get_timestamps_with_offset()
+    actual = get_timestamps_with_offset()
     assert isinstance(actual, tuple)
     assert isinstance(actual[0], str)
     assert isinstance(actual[1], str)
 
 
 def test_timestamp_with_offset_format():
-    actual = fetch_and_publish_lubw_data.get_timestamps_with_offset()
+    actual = get_timestamps_with_offset()
     datetime.strptime(actual[0], "%Y-%m-%dT%H:%M:%S")
     datetime.strptime(actual[1], "%Y-%m-%dT%H:%M:%S")
 
 
 def test_UTF8BasicAuth():
-    response = requests.Request("GET", url="https://www.google.de", auth=fetch_and_publish_lubw_data.UTF8BasicAuth("user", "password"))
+    response = requests.Request("GET", url="https://www.google.de", auth=UTF8BasicAuth("user", "password"))
     request = response.prepare()
     assert request.headers == {'Authorization': 'Basic dXNlcjpwYXNzd29yZA=='}
 
 
 def test_get_config():
-    actual = fetch_and_publish_lubw_data.get_config("./test.yaml")
+    actual = get_config("./test.yaml")
     assert actual["test"] == "value"
 
 
@@ -41,9 +40,9 @@ def test_fetch_station_data(mocker):
              'wert': 14.14}
         ]
     }
-    mocker.patch("app.src.fetch_and_publish_lubw_data.requests.get", return_value=mock_response)
+    mocker.patch("app.src.utils.requests.get", return_value=mock_response)
 
-    actual = fetch_and_publish_lubw_data.fetch_station_data("teststation",
+    actual = fetch_station_data("teststation",
                                                             ["PM10"],
                                                             '2025-05-18T17:00:00+01:00',
                                                             '2025-05-18T18:00:00+01:00')
@@ -65,10 +64,10 @@ def test_get_lubw_data_success(mocker):
              'wert': 14.14}
         ]
     }
-    mocker.patch("app.src.fetch_and_publish_lubw_data.requests.get", return_value=mock_response)
+    mocker.patch("app.src.utils.requests.get", return_value=mock_response)
 
     parameters = {'komponente': 'PM10', 'von': '2025-05-18T17:00:00', 'bis': '2025-05-18T18:00:00', 'station': 'DEBW015'}
 
-    result = fetch_and_publish_lubw_data.get_lubw_data(None, params=parameters)
+    result = get_lubw_data(None, params=parameters)
     assert result == mock_response.json.return_value
 
