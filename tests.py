37e445d0f8b18e9d00303271f8919e313bb3384a
tobias.weiland <tobias.weiland@7p-group.com>
2025-05-19 17:19:44 +0200

    add unittests

--- tests.py
+++ tests.py
@@ -2,6 +2,9 @@ import pytest
 import fetch_and_publish_lubw_data
 from fetch_and_publish_lubw_data import *
 import requests
+import pandas as pd
+from datetime import datetime
+import numpy as np
 
 
 def test_timestamp_with_offset_types():
@@ -21,6 +24,35 @@ def test_UTF8BasicAuth():
     response = requests.Request("GET", url="https://www.google.de", auth=fetch_and_publish_lubw_data.UTF8BasicAuth("user", "password"))
     request = response.prepare()
     assert request.headers == {'Authorization': 'Basic dXNlcjpwYXNzd29yZA=='}
+
+
+def test_get_config():
+    actual = fetch_and_publish_lubw_data.get_config("./test.yaml")
+    assert actual["test"] == "value"
+
+
+def test_fetch_station_data(mocker):
+    mock_response = mocker.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {
+        'station': 'Heilbronn',
+        'komponente': 'Schwebstaub PM10 kontinuierlich',
+        'einheit': 'µg/m³', 'messwerte': [
+            {'startZeit': '2025-05-18T17:00:00+01:00',
+             'endZeit': '2025-05-18T18:00:00+01:00',
+             'wert': 14.14}
+        ]
+    }
+    mocker.patch("fetch_and_publish_lubw_data.requests.get", return_value=mock_response)
+
+    actual = fetch_and_publish_lubw_data.fetch_station_data("teststation",
+                                                            ["PM10"],
+                                                            '2025-05-18T17:00:00+01:00',
+                                                            '2025-05-18T18:00:00+01:00')
+
+    expected = pd.DataFrame({"datetime": pd.Timestamp("2025-05-18T18:00:00+01:00"),
+                             "PM10": 14.14}, index=[0])
+    pd.testing.assert_frame_equal(actual, expected, check_dtype=False)
 
 
 def test_get_lubw_data_success(mocker):
