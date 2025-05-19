ae4b23af9b1c873a9e0c363cb714382108a3488c
tobias.weiland <tobias.weiland@7p-group.com>
2025-05-19 21:31:20 +0200

    add script to add custom range lubw data

---
+++ app/src/fetch_and_publish_lubw_custom.py
@@ -0,0 +1,29 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from app.src.utils import *
+
+
+def main():
+    # date format '%Y-%m-%dT%H:%M:%S' -> '2025-05-10T00:00:00+01:00'
+    start_time_str = '2025-05-10T00:00:00+01:00'
+    end_time_str = '2025-05-18T00:00:00+01:00'
+    date_range = pd.date_range(start=start_time_str, end=end_time_str, freq='h')
+    station_components = get_config("./stations.yaml")
+
+    for date_time in date_range:
+        for station in station_components.keys():
+            station_data = fetch_station_data(station, station_components[station], date_time, date_time + timedelta(hours=1))
+
+            if station_data is None:
+                logging.error(f"No data received from station: {station}")
+                return
+
+            station_data = convert_timestamps(station_data)
+            station_data = convert_values(station_data)
+            try:
+                publish_sensor_data(station_data, f"sensors/lubw-hour/{station}")
+            except Exception as e:
+                logging.info(f"Could not publish data at  time {start_time_str} for {station}, {e}")
+
+
+if __name__ == "__main__":
+    main()
