720b9683724e21422a5a0ba5b2e58fd1f5f22d78
Tobias Weiland <post@tobias-weiland.eu>
2025-06-03 21:52:06 +0200

    refactore fetch_and_publish_lubw_custom.py to bulk requests

--- app/src/fetch_and_publish_lubw_custom.py
+++ app/src/fetch_and_publish_lubw_custom.py
@@ -3,20 +3,23 @@ from app.src.utils import *
 
 def main():
     # date format '%Y-%m-%dT%H:%M:%S' -> '2025-05-10T00:00:00+01:00'
-    start_time_str = '2025-05-18T00:00:00+01:00'
-    end_time_str = '2025-05-19T00:00:00+01:00'
+    start_time_str = '2024-10-15T00:00:00+01:00'
+    end_time_str = '2025-05-25T00:00:00+01:00'
     date_range = pd.date_range(start=start_time_str, end=end_time_str, freq='h')
     station_components = get_config("./stations.yaml")
 
+    chunk_size = 100
+    chunks = [date_range[i:i + chunk_size] for i in range(0, len(date_range), chunk_size)]
+
     logging.info(f"start fetching lubw data from {start_time_str} to {end_time_str}")
 
-    for date_time in date_range:
+    for chunk in chunks:
         for station in station_components.keys():
-            station_data = fetch_station_data(station, station_components[station], date_time, date_time + timedelta(hours=1))
+            station_data = fetch_station_data(station, station_components[station], chunk[0], chunk[-1])
 
             if station_data is None:
                 logging.error(f"No data received from station: {station}")
-                return
+                continue
 
             station_data = convert_timestamps(station_data)
             station_data = convert_values(station_data)
