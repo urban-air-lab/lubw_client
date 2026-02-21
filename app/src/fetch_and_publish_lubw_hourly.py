99e3d72c4ff1cf857967b0132fc85fff713c3a7e
Tobias Weiland <post@tobias-weiland.eu>
2026-02-21 22:00:16 +0100

    add correct mqtt handling for tls route in fetch_and_publish_lubw_hourly.py

--- app/src/fetch_and_publish_lubw_hourly.py
+++ app/src/fetch_and_publish_lubw_hourly.py
@@ -1,10 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from ual.mqtt.mqtt_client import MQTTClient
+
 from app.src.utils import *
 
 
 def main():
     end_time_str, start_time_str = get_timestamps_with_offset()
     station_components = get_config("./stations.yaml")
+    # TODO: Dont generate everytime, give as parameter
+    mqtt_client: MQTTClient = MQTTClient(os.getenv("MQTT_SERVER"), int(os.getenv("MQTT_PORT")),
+                                         os.getenv("MQTT_USERNAME"), os.getenv("MQTT_PASSWORD"))
+
 
     for station in station_components.keys():
         station_data = fetch_station_data(station, station_components[station], start_time_str, end_time_str)
@@ -15,10 +21,10 @@ def main():
 
         station_data = convert_timestamps(station_data)
         station_data = convert_values(station_data)
-        try:
-            publish_sensor_data(station_data, f"sensors/lubw-hour/{station}")
-        except Exception as e:
-            logging.info(f"Could not publish data at  time {start_time_str} for {station}, {e}")
+        station_data["datetime"] = station_data["datetime"].astype(str)
+        station_data = station_data.to_dict(orient="records")
+        for element in station_data:
+            mqtt_client.publish_data(element, f"sensors/lubw-hour/{station}")
 
 
 if __name__ == "__main__":
